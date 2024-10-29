4840ac47c2e652c870c13dee4b8703b956718f32
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2024-10-29 16:18:52 -0300

    Adição do "css" ao cadastro de obras
    melhorias de estilo nos botões do cadastro de obras

--- App.js
+++ App.js
@@ -7,65 +7,69 @@ import CalendarScreen from './paginas/CalendarScreen';
 import MapScreen from './paginas/MapScreen';
 import ClockScreen from './paginas/ClockScreen';
 import Tarefas from './paginas/Tarefas';
-import CadastroEndereco from './paginas/CadastroEndereco';
-import Info from './paginas/Info'
-import Doc from './paginas/Doc'
+import Doc from './paginas/Doc';
+import Works from './paginas/Works'; // Página para "Obras"
+import Workers from './paginas/Workers'; // Página para "Funcionários"
+import CadastroObras from './paginas/CadastroObras'; // Página para "Cadastro de Obras"
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Tarefas">
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{ headerShown: true }}
-          />
-          <Stack.Screen
-            name="Calendar"
-            component={CalendarScreen}
-            options={{ headerShown: true, title: 'Calendar' }}
-          />
-          <Stack.Screen
-            name="Map"
-            component={MapScreen}
-            options={{ headerShown: true, title: 'Map' }}
-          />
-          <Stack.Screen
-            name="Clock"
-            component={ClockScreen}
-            options={{ headerShown: true, title: 'Clock' }}
-          />
-          <Stack.Screen
-            name="Info"
-            component={Info}
-            options={{ headerShown: true, title: 'Info' }}
-          />
-          <Stack.Screen
-            name="Tarefas"
-            component={Tarefas}
-            options={{ headerShown: false, title: 'Tarefas' }}
-          />
-          <Stack.Screen
-            name="Doc"
-            component={Doc}
-            options={{ headerShown: false, title: 'Documentação' }}
-          />
-          <Stack.Screen
-            name="CadastroEndereco"
-            component={CadastroEndereco}
-            options={{ headerShown: true, title: 'CadastroEndereco' }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: true, title: 'Home' }}
+        />
+        <Stack.Screen
+          name="Works"
+          component={Works}
+          options={{ headerShown: true, title: 'Obras' }}
+        />
+        <Stack.Screen
+          name="Workers"
+          component={Workers}
+          options={{ headerShown: true, title: 'Funcionários' }}
+        />
+        <Stack.Screen
+          name="Calendar"
+          component={CalendarScreen}
+          options={{ headerShown: true, title: 'Calendar' }}
+        />
+        <Stack.Screen
+          name="Map"
+          component={MapScreen}
+          options={{ headerShown: true, title: 'Map' }}
+        />
+        <Stack.Screen
+          name="Clock"
+          component={ClockScreen}
+          options={{ headerShown: true, title: 'Clock' }}
+        />
+        <Stack.Screen
+          name="Tarefas"
+          component={Tarefas}
+          options={{ headerShown: false, title: 'Tarefas' }}
+        />
+        <Stack.Screen
+          name="Doc"
+          component={Doc}
+          options={{ headerShown: false, title: 'Documentação' }}
+        />
+        <Stack.Screen
+          name="CadastroObras"
+          component={CadastroObras}
+          options={{ headerShown: true, title: 'Cadastro de Obras' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
